08b9883d9a13a06f28eeb8d8d020f3ae37935956
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2021-06-29 22:19:50 +0200

    added spring data jpa and mysql driver dependencies

---
+++ src/main/java/com/openclassrooms/paymybuddy/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.openclassrooms.paymybuddy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * The type Mvc config.
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home.html");
+        registry.addViewController("/home").setViewName("home.html");
+    }
+}
